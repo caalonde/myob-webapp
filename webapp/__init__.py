48a8e587763289e38326cc795615f5207b0f9372
Cesar Allan Alonde <allan@Cesars-MacBook-Pro-2.local>
2021-08-17 21:59:48 +1000

    modifying get app info

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -24,30 +24,35 @@ def myapp():
 
         # Git hash info
         gitcmd = "git rev-parse --short HEAD"
-        #process = subprocess.Popen(gitcmd.split(), stdout=subprocess.PIPE)
-        with subprocess.Popen(gitcmd.split(), stdout=subprocess.PIPE) as process:
-            githash, error = process.communicate()
-            print(error)
+        process = subprocess.run(gitcmd.split(), stdout=subprocess.PIPE, text=True, check=True)
+        githash = process.stdout
 
-        name="none"
-        version = "0000000000"
-        description = "no description"
-        with open("setup.py", "r") as line:
-            if "name" in line:
-                name = line.split("=")
-                name = name[1].replace("'","").replace(",","")
+        # Get app info
+        pipcmd = "pip show webapp"
+        process = subprocess.run(pipcmd.split(), stdout=subprocess.PIPE, text=True, check=True)
+        pipout = process.stdout
+
+        name = ""
+        version = ""
+        description = ""
+
+        lines = pipout.split("\n")
+
+        for line in lines:
+            if "Name" in line:
+                name = line.split(":")[1]
                 #print(name)
-            elif "version" in line:
-                version = line.split("=")
-                version = version[1].replace("'","").replace(",","")
+            elif "Version" in line:
+                print(line)
+                version = line.split(":")[1]
                 #print(version)
-            elif "description" in line:
-                description = line.split("=")
-                description = description[1].replace("'","").replace(",","")
+            elif "Summary" in line:
+                description = line.split(":")[1]
                 #print(description)
+
         data = {'description': description.strip(), \
                 'version': version.strip(), \
-                'lastcommitsha': str(githash,"utf-8")[0:7] }
+                'lastcommitsha': githash.strip() }
         fdata = { name.strip() : [ data ]}
         jdata = json.dumps(fdata)
 
